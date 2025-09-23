8f685292626d1879cb687310c16798e937d1bfb7
omartmam <omartmam9@gmail.com>
2025-09-23 14:36:03 +0300

    init project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FruitApp());
+}
+
+class FruitApp extends StatelessWidget {
+  const FruitApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
