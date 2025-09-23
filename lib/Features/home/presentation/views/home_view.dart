41ae0a47c41b0fa0a567c740fd1059aee30f93f2
omartmam <omartmam9@gmail.com>
2025-09-23 17:28:43 +0300

    create home view body + finish app bar

--- lib/Features/home/presentation/views/home_view.dart
+++ lib/Features/home/presentation/views/home_view.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fruit_app/Core/utils/app_styles.dart';
+import 'package:fruit_app/Features/home/presentation/views/widgets/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            SvgPicture.asset(
+              'assets/motor.svg',
+              width: 28,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              '61 Hopper street..',
+              style: AppStyles.secondaryText(context).copyWith(
+                fontSize: getResponsiveFontSize(context, fontSize: 22),
+              ),
+            ),
+            Expanded(child: SizedBox()),
+            SvgPicture.asset(
+              'assets/pasket_icon.svg',
+              width: 28,
+            )
+          ],
+        ),
+      ),
+      body: HomeViewBody(),
+    );
   }
 }
