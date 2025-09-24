701cb4bdadef41cc4e7ab3c053a823b2969f6256
omartmam <omartmam9@gmail.com>
2025-09-24 15:12:38 +0300

    create category item and fnish it

---
+++ lib/Features/home/presentation/views/widgets/category_item.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_app/Core/utils/app_styles.dart';
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem({super.key, required this.image, required this.text});
+  final String image, text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              shape: BoxShape.circle,
+            ),
+            child: Image.asset(
+              image,
+              width: 40,
+              height: 40,
+              fit: BoxFit.contain,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            text,
+            style: AppStyles.interSemiBold(context),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,19 +1,50 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:fruit_app/Core/utils/app_styles.dart';
+import 'package:fruit_app/Features/home/data/models/category_model.dart';
+import 'package:fruit_app/Features/home/presentation/views/widgets/category_item.dart';
 import 'package:fruit_app/Features/home/presentation/views/widgets/custom_offers_slider.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
-
-  
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        CustomOffersSlider()
+        CustomOffersSlider(),
+        SizedBox(
+          height: 10,
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: CategoryList(),
+        )
       ],
     );
   }
 }
 
+class CategoryList extends StatelessWidget {
+  const CategoryList({
+    super.key,
+  });
+
+  static List<CategoryModel> category = [
+    CategoryModel(image: 'assets/pasket.png', name: 'Fruits'),
+    CategoryModel(image: 'assets/milk.png', name: 'Milk & egg'),
+    CategoryModel(image: 'assets/food.png', name: 'Beverages'),
+    CategoryModel(image: 'assets/cloth.png', name: 'Laundry'),
+    CategoryModel(image: 'assets/vegetables.png', name: 'Vegetables'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: List.generate(category.length, (index) {
+        return CategoryItem(
+            image: category[index].image, text: category[index].name);
+      }),
+    );
+  }
+}
