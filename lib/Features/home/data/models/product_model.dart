e05f7335f7df8b8a069e7213213e1a88c3a44be4
omartmam <omartmam9@gmail.com>
2025-09-24 16:10:30 +0300

    create product model

---
+++ lib/Features/home/data/models/product_model.dart
@@ -0,0 +1,11 @@
+class ProductModel {
+  final String name, image, price, rate, rateCount;
+
+  ProductModel({
+    required this.name,
+    required this.image,
+    required this.price,
+    required this.rate,
+    required this.rateCount,
+  });
+}
